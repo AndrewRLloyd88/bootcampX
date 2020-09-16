42ad71ab9e05c1935fa8f0fae9f93705e12910fd
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-16 14:24:32 -0700

    completed exercises in bootcampX for pools and clients. Have obfuscated the passwords in this push for security

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,36 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 'xxx',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let cohortName = process.argv[2];
+let limit = process.argv[3];
+console.log(cohortName)
+console.log(limit)
+
+query = 'SELECT DISTINCT teachers.name AS teacher,' 
+query += ' cohorts.name AS cohort' 
+query += ' FROM teachers' 
+query += ' JOIN assistance_requests' 
+query += ' ON teachers.id = teacher_id' 
+query += ' JOIN students' 
+query += ' ON student_id = students.id' 
+query += ' JOIN cohorts' 
+query += ' ON cohort_id = cohorts.id' 
+query += ' WHERE cohorts.name = $1 ORDER BY teachers.name;'
+console.log(query);
+
+//SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort FROM teachers JOIN assistance_requests ON teachers.id = teacher_id JOIN students ON student_id = students.id JOIN cohorts ON cohort_id = cohorts.id WHERE cohorts.name = $1 ORDER BY teachers.name;
+
+pool.query(query, [cohortName])
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+  })
+  pool.end();
+})
+.catch(err => console.error('query error', err.stack));
